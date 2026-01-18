84a22e618d4dba3731e5eda5e4b866c08d783d5a
montenegro-cruz <montenegro.uk@icloud.com>
2026-01-18 23:29:22 +0000

    chore: add initial clean architecture structure for TechNova Store

---
+++ src/main/java/com/montenegro/technova/TechNovaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.montenegro.technova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechNovaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechNovaStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/montenegro/technova/TechNovaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montenegro.technova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechNovaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
