4713cf97ae2199962cd46659090d8d27b785a14c
montenegro-cruz <montenegro.uk@icloud.com>
2026-01-20 00:20:14 +0000

    refactor(service): integrate mapper and file storage into ProductServiceImpl

--- src/main/java/com/montenegro/technova/service/ProductServiceImpl.java
+++ src/main/java/com/montenegro/technova/service/ProductServiceImpl.java
@@ -3,6 +3,7 @@ package com.montenegro.technova.service;
 import com.montenegro.technova.dto.ProductRequestDto;
 import com.montenegro.technova.dto.ProductResponseDto;
 import com.montenegro.technova.exeption.ResourceNotFoundException;
+import com.montenegro.technova.mapper.ProductMapper;
 import com.montenegro.technova.model.Category;
 import com.montenegro.technova.model.Product;
 import com.montenegro.technova.repository.ProductRepository;
@@ -15,34 +16,24 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
 
-  private ProductRepository productRepository;
+  private final ProductRepository productRepository;
+  private final ProductMapper productMapper;
+  private final FileStorageService fileStorageService;
 
   @Override
   public ProductResponseDto createProduct(ProductRequestDto dto) {
-    // Aquí luego usaremos el mapper
-    Product product = new Product();
-    product.setName(dto.getName());
-    product.setBrand(dto.getBrand());
-    product.setModel(dto.getName());
-    product.setCategory(dto.getCategory());
-    product.setPrice(dto.getPrice());
-    product.setStock(dto.getStock());
-    product.setDescription(dto.getDescription());
-    product.setReleaseYear(dto.getReleaseYear());
+
+    Product product = productMapper.toEntity(dto);
+
+    // Guardar imagen si existe
+    if (dto.getImageFile() != null && !dto.getImageFile()
+       .isEmpty())
+    {
+      String fileName = fileStorageService.saveFile(dto.getImageFile());
+      product.setImageFileName(fileName);
+    }
     Product saved = productRepository.save(product);
-
-    return ProductResponseDto.builder()
-       .id(saved.getId())
-       .name(saved.getName())
-       .brand(saved.getBrand())
-       .model(saved.getModel())
-       .category(saved.getCategory())
-       .price(saved.getPrice())
-       .stock(saved.getStock())
-       .description(saved.getDescription())
-       .releaseYear(saved.getReleaseYear())
-       .build();
-
+    return productMapper.toResponse(saved);
 
   }
 
@@ -51,54 +42,38 @@ public class ProductServiceImpl implements ProductService {
     Product product = productRepository.findById(id)
        .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
 
-    product.setName(dto.getName());
-    product.setBrand(dto.getBrand());
-    product.setModel(dto.getModel());
-    product.setCategory(dto.getCategory());
-    product.setPrice(dto.getPrice());
-    product.setStock(dto.getStock());
-    product.setDescription(dto.getDescription());
-    product.setReleaseYear(dto.getReleaseYear());
+    // Actualizar campos
+    productMapper.updateEntity(product, dto);
 
-    Product updated = productRepository.save(product);
-
-    return ProductResponseDto.builder()
-       .id(updated.getId())
-       .name(updated.getName())
-       .brand(updated.getBrand())
-       .model(updated.getModel())
-       .category(updated.getCategory())
-       .price(updated.getPrice())
-       .stock(updated.getStock())
-       .description(updated.getDescription())
-       .releaseYear(updated.getReleaseYear())
-       .build();
+    // Si hay nueva imagen, borrar la anterior y guardar la nueva
+    if (dto.getImageFile() != null && !dto.getImageFile()
+       .isEmpty())
+    {
+      // Borrar imagen antigua
+      fileStorageService.deleteFile(product.getImageFileName());
+      // Guardar nueva imagen
+      String fileName = fileStorageService.saveFile(dto.getImageFile());
+      product.setImageFileName(fileName);
+    }
+    Product update = productRepository.save(product);
+    return productMapper.toResponse(update);
   }
 
   @Override
   public void deleteProduct(Long id) {
-    if (!productRepository.existsById(id)) {
-      throw new ResourceNotFoundException("Product not found");
-    }
-    productRepository.deleteById(id);
+    Product product = productRepository.findById(id)
+       .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+
+    // Borrar imagen asociada
+    fileStorageService.deleteFile(product.getImageFileName());
+    productRepository.delete(product);
   }
 
   @Override
   public ProductResponseDto getProductById(Long id) {
     Product product = productRepository.findById(id)
        .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
-
-    return ProductResponseDto.builder()
-       .id(product.getId())
-       .name(product.getName())
-       .brand(product.getBrand())
-       .model(product.getModel())
-       .category(product.getCategory())
-       .price(product.getPrice())
-       .stock(product.getStock())
-       .description(product.getDescription())
-       .releaseYear(product.getReleaseYear())
-       .build();
+    return productMapper.toResponse(product);
   }
 
   @Override
@@ -106,17 +81,7 @@ public class ProductServiceImpl implements ProductService {
 
     return productRepository.findAll()
        .stream()
-       .map(product -> ProductResponseDto.builder()
-          .id(product.getId())
-          .name(product.getName())
-          .brand(product.getBrand())
-          .model(product.getModel())
-          .category(product.getCategory())
-          .price(product.getPrice())
-          .stock(product.getStock())
-          .description(product.getDescription())
-          .releaseYear(product.getReleaseYear())
-          .build())
+       .map(productMapper::toResponse)
        .toList();
   }
 
@@ -124,17 +89,7 @@ public class ProductServiceImpl implements ProductService {
   public List<ProductResponseDto> getProductsByCategory(Category category) {
     return productRepository.findByCategory(category)
        .stream()
-       .map(product -> ProductResponseDto.builder()
-          .id(product.getId())
-          .name(product.getName())
-          .brand(product.getBrand())
-          .model(product.getModel())
-          .category(product.getCategory())
-          .price(product.getPrice())
-          .stock(product.getStock())
-          .description(product.getDescription())
-          .releaseYear(product.getReleaseYear())
-          .build())
+       .map(productMapper::toResponse)
        .toList();
   }
 
@@ -142,17 +97,7 @@ public class ProductServiceImpl implements ProductService {
   public List<ProductResponseDto> searchProductsByName(String name) {
     return productRepository.findByNameContainingIgnoreCase(name)
        .stream()
-       .map(product -> ProductResponseDto.builder()
-          .id(product.getId())
-          .name(product.getName())
-          .brand(product.getBrand())
-          .model(product.getModel())
-          .category(product.getCategory())
-          .price(product.getPrice())
-          .stock(product.getStock())
-          .description(product.getDescription())
-          .releaseYear(product.getReleaseYear())
-          .build())
+       .map(productMapper::toResponse)
        .toList();
   }
 }
